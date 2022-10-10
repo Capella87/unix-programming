80dc83422e1d10dacd1ac5260c494e1be85394ec
Capella87 <contact@capelladev.com>
2022-10-10 23:52:22 +0900

    Revamp 0926-1.c

--- src/Practices/0926-1.c
+++ src/Practices/0926-1.c
@@ -1,56 +1,62 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
-#include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+
+void print_file_type(struct stat* info)
+{
+    printf("File Type : ");
+    if (S_ISFIFO(info->st_mode))
+        puts("FIFO File");
+    else if (S_ISDIR(info->st_mode))
+        puts("Directory");
+    else if (S_ISREG(info->st_mode))
+        puts("Regular File");
+    else if (S_ISLNK(info->st_mode))
+        puts("Symbolic Link");
+    else puts("ETC file");
+}
+
+void print_permissions(struct stat* info)
+{
+    printf("Permission : ");
+
+    for (int i = 0; i <= 6; i += 3)
+    {
+        if (info->st_mode & (S_IREAD >> i))
+            putchar('r');
+        else putchar('-');
+
+        if (info->st_mode & (S_IWRITE >> i))
+            putchar('w');
+        else putchar('-');
+
+        if (info->st_mode & (S_IEXEC >> i))
+            putchar('x');
+        else putchar('-');
+    }
+    putchar('\n');
+}
 
 int main(int argc, char** argv)
 {
     if (argc < 2)
     {
-        printf("Not enough argument");
+        printf("An argument is not specified\n");
         exit(1);
     }
 
-    struct stat buf;
-    stat("unix.txt", &buf);
-    
     printf("File Name : %s\n", argv[1]);
-    printf("Inode Number : %lu\n", buf.st_ino);
-    printf("File Type : ");
-    
-    if (S_ISREG(buf.st_mode))
-        printf("Regular File\n");
-    else if (S_ISDIR(buf.st_mode))
-            printf("Directory\n");
-    printf("Permission : ");
-    char perm[10] = "------------";
+    struct stat info;
 
-    if ((buf.st_mode & S_IRUSR) != 0)
-        perm[1] = 'r';
-    if ((buf.st_mode & S_IWUSR) != 0)
-        perm[2] = 'w';
-    if ((buf.st_mode & S_IXUSR) != 0)
-        perm[3] = 'x';
-    if ((buf.st_mode & S_IRGRP) != 0)
-        perm[4] = 'r';
-    if ((buf.st_mode & S_IWGRP) != 0)
-        perm[5] = 'w';
-    if ((buf.st_mode & S_IXGRP) != 0)
-        perm[6] = 'x';
-    if ((buf.st_mode & S_IROTH) != 0)
-        perm[7] = 'r';
-    if ((buf.st_mode & S_IWOTH) != 0)
-        perm[8] = 'w';
-    if ((buf.st_mode & S_IXOTH) != 0)
-        perm[9] = 'x';
-        
-    printf("%s\n", perm);
-
-    printf("UID : %d\n", buf.st_uid);
-    printf("Size : %ld\n", buf.st_size);
+    stat(argv[1], &info);
+    printf("Inode Number : %lu\n", info.st_ino);
+    print_file_type(&info);
+    print_permissions(&info);
+    printf("UID : %d\n", info.st_uid);
+    printf("Size : %ld\n", info.st_size);
 
     return 0;
 }
