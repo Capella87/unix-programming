d1f5851615069d2e74d688d15a4e7c17c7a2777d
Capella87 <contact@capelladev.com>
2022-11-15 20:09:34 +0900

    Add 1102-4.c

---
+++ src/Practices/1102-4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+void handler(int signo)
+{
+    printf("Signal is detected\n");
+    psignal(signo, "Received");
+}
+
+int main(void)
+{
+    struct sigaction act;
+
+    sigfillset(&act.sa_mask);
+    sigdelset(&act.sa_mask, SIGUSR1);
+    act.sa_flags = 0;
+    act.sa_handler = handler;
+
+    sigaction(SIGUSR1, &act, (struct sigaction*)0);
+    pause();
+
+    return 0;
+}
