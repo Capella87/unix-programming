c2f04c060fd7ccfc9ce925decd4a26c40ea522a8
Capella87 <contact@capelladev.com>
2022-11-28 16:05:17 +0900

    Add 1128-1

---
+++ src/Practices/1128-1-client.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(void)
+{
+    key_t key;
+    int shm_id;
+    
+    shm_id = shmget(1000, 1024, IPC_PRIVATE | 0644);
+    if (shm_id == -1)
+    {
+        perror("shmget");
+        exit(1);
+    }
+
+    char* address = (char*)shmat(shm_id, (char*)0, 0);
+
+    for (int i = 0; i < 10; i++)
+    {
+        strcpy(address, "PROGRAMMING");
+        sleep(1);
+        puts(address);
+    }
+
+    shmdt((char*)address);
+    shmctl(shm_id, IPC_RMID, (struct shmid_ds*)0);
+
+    return 0;
+}
